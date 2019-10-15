48d95819b2a80dcdd7862b8a9798d804984b88cd
Emir Demirel <e.demirel@qmul.ac.uk>
2019-10-15 14:42:22 +0200

    Update path.sh

--- path.sh
+++ path.sh
@@ -1,5 +1,7 @@
 #export KALDI_ROOT=`pwd`/../../..
-export KALDI_ROOT=/home/emir/QMUL/gitRepos/kaldi
+#export KALDI_ROOT=/home/emir/QMUL/gitRepos/kaldi
+#Modify and uncomment below to automatically run kaldi binaries
+#KALDI_ROOT='/path/to/kaldi'
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
