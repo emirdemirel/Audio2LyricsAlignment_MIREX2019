19626986c595455e171661ac86cd23f3e8eb2863
emir demirel <emir.demirel@upf.edu>
2019-10-27 01:27:26 +0200

    debugged for different datasets

--- audio2lyricsalignment.py
+++ audio2lyricsalignment.py
@@ -10,14 +10,11 @@ def main(args):
     out_dir = args.out_dir
     db_name = args.db_name
     
-    #Retrieve directory paths to recordings and lyrics to link for the pipeline processing --> Linking may not be necessary / TODO!!!
-    #rec_dir = '/'.join(rec_path.split('/')[:-1]) 
-    #lyrics_dir = '/'.join(lyrics_path.split('/')[:-1]) 
-    
     #You can change the stage of the whole process if you want
-    #To skip Data Preparation set stage = ...
-    #To skip Feature Extraction set stage = ...
-    #To skip .... (TODO)
+    #Stage 1 : Data Preparation
+    #Stage 2 : Feature Extraction
+    #Stage 3 : Forced Alignment
+    #Stage 4 : Data Reformat & Post-process  
     stage = 0
     #Command the run the alignment pipeline
     cmd_run='./run.sh ' + rec_path + ' ' + lyrics_path + ' ' + out_dir + ' ' + db_name #+ ' --stage ' + str(stage)
@@ -31,9 +28,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("rec_path", type=str, help="Path to target audio recording", default="wav")
     parser.add_argument("lyrics_path", type=str, help="Path to lyrics (.txt) file for corresponding audio recording", default="lyrics")
-    parser.add_argument("db_name", type=str, help="name of the dataset", default ="hansen")
     parser.add_argument("out_dir", type=str, help="Path where the output alignment files will be saved", default ="alignments")
-    #parser.add_argument("db_path", type=str, help="Path to JamendoLyrics repository")
+    parser.add_argument("db_name", type=str, help="name of the dataset", default ="hansen")
 
     args = parser.parse_args()
     main(args)
