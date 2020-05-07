6b0fb5fa3defc6f765f2f739e3e4dd880a712e92
Emir Demirel <e.demirel@qmul.ac.uk>
2020-05-07 13:24:26 +0200

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# "queue.pl" uses qsub.  The options to it are
-# options to qsub.  If you have GridEngine installed,
-# change this to a queue you have access to.
-# Otherwise, use "run.pl", which will run jobs locally
-# (make sure your --num-jobs options are no more than
-# the number of cpus on your machine.
-
-# This cmd.sh file is to be used on Sharc in RSE queue
 
 export train_cmd="run.pl --max-jobs-run 3"
 export decode_cmd="run.pl --max-jobs-run 3"
